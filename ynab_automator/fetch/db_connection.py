d14358254593139d5fb5887ac0e242228d3fcf07
metthis <matyas.vilimek@gmail.com>
2023-06-18 18:12:29 +0300

    remove unused imports from db_connection.py

--- ynab_automator/fetch/db_connection.py
+++ ynab_automator/fetch/db_connection.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
-from collections.abc import Iterable
 import sqlite3
-import textwrap
 
 from ynab_automator.config import ignored
 from ynab_automator.db import path
