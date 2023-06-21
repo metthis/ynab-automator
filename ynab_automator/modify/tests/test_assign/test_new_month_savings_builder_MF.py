286a2b1c85d18e98695cb2874f374b00f603169f
metthis <matyas.vilimek@gmail.com>
2023-06-22 00:09:33 +0300

    add test for assign.new_month_savings_builder_MF

---
+++ ynab_automator/modify/tests/test_assign/test_new_month_savings_builder_MF.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from random import randint
+
+import pytest
+
+from ynab_automator.datastructures.ynab_dataclasses import MonthCategory
+from ynab_automator.modify import assign
+
+
+# cases is a list made up of tuples, with each tuple made up of 2 identical dictionaries.
+# The dictionaries have to be doubled like this because it seems to be the only way
+# to pass each dictionary both to the fixture (called: m_category)
+# and as a variable to the test itself (called: data).
+# This approach will be used in other tests as well.
+
+cases = (
+    {
+        "budgeted": randint(0, 499),
+        "goal_overall_left": randint(0, 499),
+    }
+    for _ in range(5)
+)
+cases = [(x, x) for x in cases]
+
+
+@pytest.mark.parametrize("m_category, data", cases, indirect=["m_category"])
+def test_new_month_savings_builder_MF(m_category: MonthCategory, data: dict):
+    result = assign.new_month_savings_builder_MF(m_category)
+    assert isinstance(result, tuple)
+    assert len(result) == 1
+
+    resulted_m_category = result[0]
+    assert isinstance(resulted_m_category, MonthCategory)
+    assert (
+        resulted_m_category.new_budgeted == data["budgeted"] + data["goal_overall_left"]
+    )
