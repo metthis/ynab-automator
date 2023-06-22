fde9c77af13521ad89bc5638e16af9b9d11401c2
metthis <matyas.vilimek@gmail.com>
2023-06-22 08:51:21 +0300

    add test for assign.new_month_savings_balance_TB

--- ynab_automator/modify/tests/test_assign/test_savings_balance_TB.py
+++ ynab_automator/modify/tests/test_assign/test_savings_balance_TB.py
@@ -40,7 +40,7 @@ notes = [
 notes = [textwrap.dedent(x) for x in notes]
 
 
-expected_results = [
+expected_limits = [
     10_000,
     20_000,
     30_000,
@@ -49,7 +49,7 @@ expected_results = [
 ]
 
 
-cases = list(zip(notes, expected_results, strict=True))
+cases = list(zip(notes, expected_limits, strict=True))
 
 
 @pytest.mark.parametrize("note, expected", cases)
@@ -58,8 +58,48 @@ def test_extract_TB_limit(note: str, expected: int | float):
     assert result == expected
 
 
-# cases is a list made up of tuples, with each tuple made up of 2 identical dictionaries.
+# cases is a list made up of tuples, with each tuple made up of (among other things) 2 identical dictionaries.
 # The dictionaries have to be doubled like this because it seems to be the only way
 # to pass each dictionary both to the fixture (called: m_category)
 # and as a variable to the test itself (called: data).
 # This approach will be used in other tests as well.
+
+
+cases = [
+    {
+        "budgeted": randint(0, 100_000),
+        "goal_overall_left": randint(0, 100_000),
+    }
+    for _ in range(5)
+]
+
+
+for data, note in zip(cases, notes, strict=True):
+    data["note"] = note
+
+
+cases = [
+    (data, data, limit) for data, limit in zip(cases, expected_limits, strict=True)
+]
+
+
+@pytest.mark.parametrize(
+    "m_category, data, expected_limit", cases, indirect=["m_category"]
+)
+def test_new_month_savings_balance_TB(
+    m_category: MonthCategory, data: dict, expected_limit: int | float
+):
+    result = assign.new_month_savings_balance_TB(m_category)
+    assert isinstance(result, tuple)
+    assert len(result) == 1
+
+    resulted_m_category = result[0]
+    assert isinstance(resulted_m_category, MonthCategory)
+    assert resulted_m_category.new_budgeted == data["budgeted"] + min(
+        data["goal_overall_left"], expected_limit
+    )
+    assert 0 <= resulted_m_category.delta() <= expected_limit
+    if data["goal_overall_left"] >= expected_limit:
+        assert resulted_m_category.delta() == expected_limit
+    else:
+        assert resulted_m_category.delta() == data["goal_overall_left"]
